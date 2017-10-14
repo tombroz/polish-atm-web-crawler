e439a054d800df82d1b8c8af42a9e10ea181cf82
Tomasz Brzozowski <tomek531b@gmail.com>
2017-10-14 16:34:32 +0200

    Replaced selenium with requests. Added regex to get_atm_data function. Added console output.

--- crawl_script.py
+++ crawl_script.py
@@ -3,7 +3,6 @@ import re
 import time
 import bs4
 import requests
-from selenium import webdriver
 import winsound
 
 base_atm_url = "http://www.karty.pl/bankomaty.php?bankomat="
@@ -34,9 +33,9 @@ notif_freq_fail = int(float(220)/(9.0/8.0))  # Hz
 def get_region_cities(region_url):
     region_cities = []
     # prepare html data from url
-    driver = webdriver.PhantomJS(driver_path)
-    driver.get(region_url)
-    soup = bs4.BeautifulSoup(driver.page_source, "html.parser")
+    response = requests.get(region_url)
+    html = response.text
+    soup = bs4.BeautifulSoup(html, "html.parser")
     if soup.find('title', string="503 Service Unavailable"):
         return None
     content = soup.find_all('ul')[-1] \
@@ -49,31 +48,62 @@ def get_region_cities(region_url):
 def get_city_atms_numbers(city_url):
     city_atms_numbers = []
     # prepare html data from url
-    driver = webdriver.PhantomJS(driver_path)
-    driver.get(city_url)
-    soup = bs4.BeautifulSoup(driver.page_source, "html.parser")
+    response = requests.get(city_url)
+    html = response.text
+    soup = bs4.BeautifulSoup(html, "html.parser")
     if soup.find('title', string="503 Service Unavailable"):
         return None
     # get city atms numbers from url
-    content = soup.find_all(href=re.compile("bankomat="))
+    # content = soup.find_all(href=re.compile("bankomat="))
+    # for element in content:
+    #     city_atms_numbers.append(element.get('href')[10:])
+    content = soup.find_all('dd')
     for element in content:
-        city_atms_numbers.append(element.get('href')[10:])
+        city_atms_numbers.append(element.find('a').get('href')[10:])
     return city_atms_numbers
 
 
-def get_atm_data(atm_url):
+def get_atm_data(atm_url,region_id):
     atm_data = []
     # prepare html data from url
-    driver = webdriver.PhantomJS(driver_path)
-    driver.get(atm_url)
-    soup = bs4.BeautifulSoup((driver.page_source).encode('utf-8'), "html.parser")
+    response = requests.get(atm_url)
+    response.encoding = 'utf-8'
+    html = response.text
+    soup = bs4.BeautifulSoup(html, "html.parser")
     if soup.find('title', string="503 Service Unavailable"):
         return None
 
     # get atm's data from url
     content = soup.find_all('dd')
-    for element in content:
-        atm_data.append(element.get_text())
+    content_ids = soup.find_all('dt')
+    ids_strings = []
+    for el in content_ids:
+        ids_strings.append(el.get_text())
+    for i in range(8):
+        try:
+            if i == 0:
+                atm_data.append(region_urls[region_id][46:])
+                continue
+            if i == 1:
+                ind = [j for j, item in enumerate(ids_strings) if re.search('Miejsco', item)][0]
+            elif i ==2:
+                ind = [j for j, item in enumerate(ids_strings) if re.search('Lokaliz', item)][0]
+            elif i ==3:
+                ind = [j for j, item in enumerate(ids_strings) if re.search('Bankomat nale', item)][0]
+            elif i ==4:
+                ind = [j for j, item in enumerate(ids_strings) if re.search('Dost', item)][0]
+            elif i ==5:
+                ind = [j for j, item in enumerate(ids_strings) if re.search('Numer banko', item)][0]
+            elif i ==6:
+                ind = [j for j, item in enumerate(ids_strings) if re.search('Funkcja depo', item)][0]
+            elif i ==7:
+                ind = [j for j, item in enumerate(ids_strings) if re.search('Produ', item)][0]
+        except IndexError:
+            ind=-1
+        if ind is not -1:
+            atm_data.append(content[ind].get_text())
+        else:
+            atm_data.append("null")
     return atm_data
 
 
@@ -112,14 +142,14 @@ def get_all_atms_data_rec(start_point, end_point):
                     continue
                 print("Obtaining data for: region{} = {}\t| city{} = {}\t| atm{} = {}"
                       .format(i, region_urls[i][46:], j, region_cities[j], k, city_atms_numbers[k]))
-                atm_data = get_atm_data(base_atm_url + city_atms_numbers[k])
+                atm_data = get_atm_data(base_atm_url + city_atms_numbers[k],i)
                 if atm_data is None:
                     return result_file_output(i, j, k, True)
                 with open("all_atms_data.txt", 'a', encoding='utf-8') as atms_data_file:
                     for x in range(len(atm_data)):
                         atms_data_file.write(atm_data[x])
                         if x != len(atm_data) - 1:
-                            atms_data_file.write(",")
+                            atms_data_file.write("|")
                     atms_data_file.write("\n")
                 time.sleep(float(random.randint(sleep_min, sleep_max)) * sleep_coeff)
     return result_file_output(i, j, k, False)
@@ -128,15 +158,13 @@ def get_all_atms_data_rec(start_point, end_point):
 def result_file_output(i, j, k, user_blocked):
     with open("result_end_point.txt", 'a', encoding='utf-8') as result_file:
         if user_blocked:
-            result_file.write(
-                """Blocked. From/to: (region,city,atm_nr) = {},{},{} / {},{},{}\n""".format(st[0], st[1], st[2], i, j,
-                                                                                            k))
+            output = 'Blocked. From/to: (region,city,atm_nr) = {},{},{} / {},{},{}\n'.format(st[0], st[1], st[2], i, j, k)
             sound_notif(0)
         else:
-            result_file.write(
-                """Successful. From/to: (region,city,atm_nr) = {},{},{} / {},{},{}\n""".format(st[0], st[1], st[2], i,
-                                                                                               j, k))
+            output = 'Successful. From/to: (region,city,atm_nr) = {},{},{} / {},{},{}\n'.format(st[0], st[1], st[2], i, j, k)
             sound_notif(1)
+        result_file.write(output)
+        print(output)
 
 
 def gen_sound(freq, dur, wait_after=0.0, count=1):
@@ -167,11 +195,11 @@ def sound_notif(succ):
 # st = starting region,city, atm. This should be the same as region,city,atm
 #      from last failed request.
 # e = end region id. Alphabetically. Example: dolnoslaskie = 0,...
-st = (0, 39, 9)
+st = (0, 0, 0)
 e = 1
 
-sleep_min = 1
-sleep_max = 3
-sleep_coeff = 0.1
+sleep_min = 10
+sleep_max = 20
+sleep_coeff = 0.01
 
 get_all_atms_data_rec(st, e)
