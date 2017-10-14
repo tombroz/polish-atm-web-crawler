41243f228f79d0fb1c89bcc755d926a9025fd53f
Tomasz Brzozowski <tomek531b@gmail.com>
2017-10-14 21:50:17 +0200

    Corrected control points. Always end >= start.

--- crawl_script.py
+++ crawl_script.py
@@ -24,10 +24,14 @@ region_urls = (base_region_url + "dolnoslaskie",
                base_region_url + "warminsko-mazurskie",
                base_region_url + "wielkopolskie",
                base_region_url + "zachodniopomorskie")
+
 driver_path = "C:/phantomjs-2.1.1-windows/bin/phantomjs"
 notif_dur = 500  # millisecond
 notif_freq_success = 600  # Hz
-notif_freq_fail = int(float(220)/(9.0/8.0))  # Hz
+notif_freq_fail = int(float(220) / (9.0 / 8.0))  # Hz
+sleep_min = 10
+sleep_max = 20
+sleep_coeff = 0.01
 
 
 def get_region_cities(region_url):
@@ -63,7 +67,7 @@ def get_city_atms_numbers(city_url):
     return city_atms_numbers
 
 
-def get_atm_data(atm_url,region_id):
+def get_atm_data(atm_url, region_id):
     atm_data = []
     # prepare html data from url
     response = requests.get(atm_url)
@@ -86,20 +90,20 @@ def get_atm_data(atm_url,region_id):
                 continue
             if i == 1:
                 ind = [j for j, item in enumerate(ids_strings) if re.search('Miejsco', item)][0]
-            elif i ==2:
+            elif i == 2:
                 ind = [j for j, item in enumerate(ids_strings) if re.search('Lokaliz', item)][0]
-            elif i ==3:
+            elif i == 3:
                 ind = [j for j, item in enumerate(ids_strings) if re.search('Bankomat nale', item)][0]
-            elif i ==4:
+            elif i == 4:
                 ind = [j for j, item in enumerate(ids_strings) if re.search('Dost', item)][0]
-            elif i ==5:
+            elif i == 5:
                 ind = [j for j, item in enumerate(ids_strings) if re.search('Numer banko', item)][0]
-            elif i ==6:
+            elif i == 6:
                 ind = [j for j, item in enumerate(ids_strings) if re.search('Funkcja depo', item)][0]
-            elif i ==7:
+            elif i == 7:
                 ind = [j for j, item in enumerate(ids_strings) if re.search('Produ', item)][0]
         except IndexError:
-            ind=-1
+            ind = -1
         if ind is not -1:
             atm_data.append(content[ind].get_text())
         else:
@@ -107,61 +111,69 @@ def get_atm_data(atm_url,region_id):
     return atm_data
 
 
-def get_all_atms_data_rec(start_point, end_point):
+def get_all_atms_data_rec(st_p, end_p):
     """
     Downloads atms data from karty.pl from starting region, city, atm_nr
     until given region (end_point) is reached.
     Writes real start and end points ((region, city, atm_nr) ids) to file
-    :param start_point: tuple containing (region, city, atm_nr) ids from which
+    :param st_p: tuple containing (region, city, atm_nr) ids from which
     to start downloading data
-    :param end_point: region id (from region_urls) to which downloading is continued
+    :param end_p: region id (from region_urls) to which downloading is continued
     :return: None
     """
     i, j, k = 0, 0, 0
     for i in range(len(region_urls)):
-        if i < start_point[0]:
+        if i < st_p[0]:
             continue
-        elif i >= end_point:
+        elif i >= end_p:
             break
-        print("Obtaining data for: region{} = {}".format(i, region_urls[i][46:]))
+        print("Pobieranie dla: wojew_{} = {}".format(i, region_urls[i][46:]))
         region_cities = get_region_cities(region_urls[i])
         if region_cities is None:
-            return result_file_output(i, j, k, True)
+            return result_file_output(st_p,i, j, k, True)
         time.sleep(float(random.randint(sleep_min, sleep_max)) * sleep_coeff)
         for j in range(len(region_cities)):
-            if j < start_point[1] and i == st[0]:
+            if j < st_p[1] and i == st_p[0]:
                 continue
-            print("Obtaining data for: region{} = {}\t| city{} = {}"
+            print("Pobieranie dla: wojew_{} = {}\t| miasto_{} = {}"
                   .format(i, region_urls[i][46:], j, region_cities[j]))
             city_atms_numbers = get_city_atms_numbers(base_city_url + region_cities[j])
             if city_atms_numbers is None:
-                return result_file_output(i, j, k, True)
+                return result_file_output(st_p,i, j, k, True)
             time.sleep(float(random.randint(sleep_min, sleep_max)) * sleep_coeff)
             for k in range(len(city_atms_numbers)):
-                if k < start_point[2] and j == st[1]:
+                if k < st_p[2] and j == st_p[1]:
                     continue
-                print("Obtaining data for: region{} = {}\t| city{} = {}\t| atm{} = {}"
+                print("Pobieranie dla: wojew_{} = {}\t| miasto_{} = {}\t| nr_bankom_{} = {}"
                       .format(i, region_urls[i][46:], j, region_cities[j], k, city_atms_numbers[k]))
-                atm_data = get_atm_data(base_atm_url + city_atms_numbers[k],i)
+                atm_data = get_atm_data(base_atm_url + city_atms_numbers[k], i)
                 if atm_data is None:
-                    return result_file_output(i, j, k, True)
-                with open("all_atms_data.txt", 'a', encoding='utf-8') as atms_data_file:
+                    return result_file_output(st_p,i, j, k, True)
+                with open("bankomaty_dane.txt", 'a', encoding='utf-8') as atms_data_file:
                     for x in range(len(atm_data)):
                         atms_data_file.write(atm_data[x])
                         if x != len(atm_data) - 1:
                             atms_data_file.write("|")
                     atms_data_file.write("\n")
                 time.sleep(float(random.randint(sleep_min, sleep_max)) * sleep_coeff)
-    return result_file_output(i, j, k, False)
+    return result_file_output(st_p,i, j, k, False)
 
 
-def result_file_output(i, j, k, user_blocked):
-    with open("result_end_point.txt", 'a', encoding='utf-8') as result_file:
+def result_file_output(st_p,i, j, k, user_blocked):
+    if i < st_p[0]:
+        i = st_p[0]
+    if j < st_p[1]:
+        j = st_p[2]
+    if k < st_p[2]:
+        k = st_p[2]
+    with open("punkty_kontrolne.txt", 'a', encoding='utf-8') as result_file:
         if user_blocked:
-            output = 'Blocked. From/to: (region,city,atm_nr) = {},{},{} / {},{},{}\n'.format(st[0], st[1], st[2], i, j, k)
+            output = 'Zablokowano IP. Pobrano dane od/do: (wojew|miasto|nr_bankom) = |{}|{}|{}| / |{}|{}|{}|\n'.format(st_p[0], st_p[1], st_p[2], i, j,
+                                                                                             k)
             sound_notif(0)
         else:
-            output = 'Successful. From/to: (region,city,atm_nr) = {},{},{} / {},{},{}\n'.format(st[0], st[1], st[2], i, j, k)
+            output = 'Zakonczono pobieranie. Pobrano dane od/do: (wojew|miasto|nr_bankom) = |{}|{}|{}| / |{}|{}|{}|\n'.format(st_p[0], st_p[1], st_p[2], i,
+                                                                                                j, k)
             sound_notif(1)
         result_file.write(output)
         print(output)
@@ -175,31 +187,71 @@ def gen_sound(freq, dur, wait_after=0.0, count=1):
 
 def sound_notif(succ):
     if succ:
-        gen_sound(notif_freq_success, int(float(notif_dur) / 2.0), 0.1,3)
+        gen_sound(notif_freq_success, int(float(notif_dur) / 2.0), 0.1, 3)
     else:
         notif_freq_fail_low = int(float(notif_freq_fail) / (4.0 / 3.0))
         notif_freq_fail_high = int(float(notif_freq_fail) * 6.0 / 5.0)
         short_dur = int(float(notif_dur) / 4.0)
-        medium_dur = int(float(notif_dur) / (4.0/3.0))
+        medium_dur = int(float(notif_dur) / (4.0 / 3.0))
         gen_sound(notif_freq_fail, notif_dur, 0.1, 3)
         gen_sound(notif_freq_fail_low, medium_dur)
         gen_sound(notif_freq_fail_high, (short_dur))
         gen_sound(notif_freq_fail, (notif_dur), 0.1)
-        gen_sound(notif_freq_fail_low, (medium_dur))
-        gen_sound(notif_freq_fail_high, (short_dur))
-        gen_sound(notif_freq_fail, (notif_dur), 0.1)
 
-
-# 4/3, 6/5
 
 # st = starting region,city, atm. This should be the same as region,city,atm
 #      from last failed request.
-# e = end region id. Alphabetically. Example: dolnoslaskie = 0,...
-st = (0, 0, 0)
-e = 1
+# end = end region id. Alphabetically. Example: dolnoslaskie = 0,...
+print("""Program pobiera dane bankomatow ze strony karty.pl. Nalezy podac punkt startowy w postaci:
+a,b,c
+,gdzie 'a,b,c' to liczby oznaczajace indeks wojewodztwa(a), miasta(b) i bankomatu(c), 
+od ktorego ma sie rozpoczac pobieranie danych. Indeksy wojewodztw sa podane alfabetycznie tj.:
 
-sleep_min = 10
-sleep_max = 20
-sleep_coeff = 0.01
+0	dolnoslaskie
+1	kujawsko-pomorskie
+2	lubelskie
+3	lubuskie
+4	lodzkie
+5	malopolskie
+6	mazowieckie
+7	opolskie
+8	podkarpackie
+9	podlaskie
+10	pomorskie
+11	slaskie
+12	swietokrzyskie
+13	warminsko-mazurskie
+14	wielkopolskie
+15	zachodniopomorskie
 
-get_all_atms_data_rec(st, e)
+Indeksy miast i bankomatow nie sa z gory znane i program musi pobierac je na biezaco ze strony.
+Lepiej zostawic je na 0,0 dopoki nie uzyska sie punktu kontrolnego (patrz dalej).
+    Przykladowo rozpoczecie od poczatku wojewodztwa dolnoslaskiego to podanie: 0,0,0
+Program pobiera dane dla wojewodztw dopoki nie zostanie zablokowane IP, co jest sygnalizowane dzwiekowo. 
+Wtedy nalezy zmienic IP poza programem. Następnie mozna wpisac "p" co spowoduje kontynuowanie
+pobierania od miejsca przerwania.
+    Program zapisuje wyniki w pliku tekstowym "bankomaty_dane.txt" w postaci:
+wojewodztwo	miasto|lokalizacja|wlasciciel|dostepnosc|nr_bankomatu|funkcje_depozytowe|producent
+W pliku "punkty_kontrolne.txt" znajduja sie punkty w ktorych zaczynalo i konczylo sie pobieranie.
+""")
+while 1:
+    print('Podaj po przecinku startowe: wojewodztwo(0-15), miasto(liczba), bankomat(liczba). '
+          'Wpisz "p", aby zaczac od miejsca gdzie skonczono:')
+    inp = input()
+    if inp == 'p':
+        with open("punkty_kontrolne.txt", 'r') as in_file:
+            str_st = in_file.readlines()[-1].split('|')[-4:-1]
+            try:
+                st = (int(str_st[0]), int(str_st[1]), int(str_st[2]))
+                end = 15  # st[0] + 1
+                get_all_atms_data_rec(st, end)
+            except IndexError:
+                print("Nie odczytano punktu startowego z pliku punkty_kontrolne.txt.")
+    else:
+        st = inp.split(',')
+        print(st)
+        end = 15 #int(st[0]) + 1
+        try:
+            get_all_atms_data_rec((int(st[0]), int(st[1]), int(st[2])), end)
+        except:
+            print("Nieprawidlowe dane. Wpisz 3 liczby oddzielone przecinkami.")
